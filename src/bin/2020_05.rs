b14b84f599da6ff1cf7ada4293443c9cacd96160
Tom Willemsen <tomwillemsen1995@gmail.com>
2023-07-29 18:54:52 +0100

    2020-06

--- src/bin/2020_05.rs
+++ src/bin/2020_05.rs
@@ -1,7 +1,7 @@
 use clap::Parser;
-use std::fs;
-use std::cmp::{max, min};
 use fnv::FnvHashSet;
+use std::cmp::{max, min};
+use std::fs;
 
 #[derive(Parser)]
 struct Cli {
@@ -9,29 +9,22 @@ struct Cli {
     input: String,
 }
 
-fn parse(raw_inp: &String) -> Vec<&str> {
+fn parse(raw_inp: &str) -> Vec<&str> {
     raw_inp.trim().split('\n').collect()
 }
 
 fn get_seat_id(seat: &str) -> u64 {
-    let mut row: u64 = 0;
-    let mut col: u64 = 0;
-    
+    let mut id: u64 = 0;
+
     let chars: Vec<char> = seat.chars().collect();
-    
-    for i in 0..7 as usize {
-        if chars[i] == 'B' {
-            row += 1 << (6-i);
+
+    for (i, item) in chars.iter().enumerate().take(10) {
+        if item == &'B' || item == &'R' {
+            id += 1 << (9 - i);
         }
     }
-    
-    for j in 7..10 as usize {
-        if chars[j] == 'R' {
-            col += 1 << (9-j);
-        }
-    }
-    
-    row * 8 + col
+
+    id
 }
 
 fn main() {
@@ -40,26 +33,26 @@ fn main() {
     let raw_inp = fs::read_to_string(args.input).expect("can't open input file");
 
     let data = parse(&raw_inp);
-    
+
     let mut max_seat = u64::MIN;
     let mut min_seat = u64::MAX;
-    
+
     let mut occupied_seats: FnvHashSet<u64> = FnvHashSet::default();
-    
+
     for item in data.iter() {
         if item.len() != 10 {
-            panic!("invalid data");
+            panic!("invalid data length");
         }
         let seat_id = get_seat_id(item);
         max_seat = max(max_seat, seat_id);
         min_seat = min(min_seat, seat_id);
         occupied_seats.insert(seat_id);
     }
-    
+
     let p1 = max_seat;
     let mut p2 = 0;
-    
-    for s in (min_seat+1)..max_seat {
+
+    for s in (min_seat + 1)..max_seat {
         if !occupied_seats.contains(&s) {
             p2 = s;
             break;
---
+++ src/bin/2020_06.rs
@@ -0,0 +1,70 @@
+use clap::Parser;
+use fnv::FnvHashSet;
+use std::fs;
+
+#[derive(Parser)]
+struct Cli {
+    #[clap(short, long)]
+    input: String,
+}
+
+fn parse(raw_inp: &str) -> Vec<Vec<&str>> {
+    raw_inp
+        .trim()
+        .split("\n\n")
+        .map(|item| item.split('\n').map(|l| l.trim()).collect())
+        .collect()
+}
+
+fn calculate(data: &[Vec<&str>]) -> (usize, usize) {
+    let mut p1 = 0;
+    let mut p2 = 0;
+
+    for group in data.iter() {
+        let mut hs: FnvHashSet<char> = FnvHashSet::default();
+        for person in group.iter() {
+            for c in person.chars() {
+                hs.insert(c);
+            }
+        }
+        p1 += hs.len();
+
+        for item in hs.iter() {
+            if group.iter().all(|person| person.contains(|c| &c == item)) {
+                p2 += 1;
+            }
+        }
+    }
+    (p1, p2)
+}
+
+fn main() {
+    let args = Cli::parse();
+    let raw_inp = fs::read_to_string(args.input).expect("can't open input file");
+    let data = parse(&raw_inp);
+    let (p1, p2) = calculate(&data);
+    println!("{}\n{}", p1, p2);
+}
+
+#[test]
+fn test_example() {
+    let example_data = "abc
+
+        a
+        b
+        c
+
+        ab
+        ac
+
+        a
+        a
+        a
+        a
+
+        b";
+
+    let (p1, p2) = calculate(&parse(&example_data));
+    assert_eq!(p1, 11);
+    assert_eq!(p2, 6);
+}
